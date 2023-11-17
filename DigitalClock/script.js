ece641ca26cf8e5e0016eb07c778125e41a9f6f8
Neeraj <neerajjoshi9389798147@gmail.com>
2023-11-17 10:01:56 +0530

    3rd done

---
+++ DigitalClock/script.js
@@ -0,0 +1,19 @@
+function clock(){
+    let hours=document.getElementById('hours')
+    let minutes=document.getElementById('minutes')
+    let seconds=document.getElementById('seconds')
+    let period =document.getElementById('period')
+
+let h=new Date().getHours()
+let m=new Date().getMinutes()
+let s=new Date().getSeconds()
+let ampm= h>=12 ? "PM":"AM"
+
+hours.innerHTML=h
+minutes.innerHTML=m
+seconds.innerHTML=s
+period.innerHTML=ampm
+
+
+}
+ setInterval(clock,1000) ;
